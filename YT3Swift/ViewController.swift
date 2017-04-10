bda6cff3402713a67991d927378d84bfbd51b8a7
Jake Spann <jake323@comcast.net>
2017-04-10 09:32:30 -0500

    Initial Commit

---
+++ YT3Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YT3Swift
+//
+//  Created by Jake Spann on 4/10/17.
+//  Copyright © 2017 Jake Spann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
