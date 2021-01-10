8bee35f1274dd2336527136cd525adf4dd2d250f
Jake Spann <jake323@comcast.net>
2021-01-09 18:26:22 -0600

    Improvements

--- YT3Swift/View Controllers/FormatControlsVC.swift
+++ YT3Swift/View Controllers/FormatControlsVC.swift
@@ -16,12 +16,34 @@ class FormatControlsVC: NSViewController {
     @IBOutlet weak var formatsPopUpButton: NSPopUpButton!
     @IBOutlet weak var resolutionPopUpButton: NSPopUpButton!
     
-    var formatList: [String]?
+    var formatList: [MediaFormat]?
+    
+    var extensionList: [String]?
     var resolutionList: [String]?
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+    }
+    
+    @IBAction func selectedFormat(_ sender: NSPopUpButton) {
+        let availableResolutions = (formatList?.filter({$0.fileExtension.rawValue == sender.titleOfSelectedItem}).map({$0.sizeString}) ?? []) + ["Auto"]
+        
+        //print(availableResolutions)
+        
+        if !availableResolutions.contains(resolutionPopUpButton.titleOfSelectedItem) {
+            print("INCOMPATIBLE: \(sender.titleOfSelectedItem) - \(resolutionPopUpButton.titleOfSelectedItem)")
+        }
+    }
+    
+    @IBAction func selectedExtension(_ sender: NSPopUpButton) {
+        let availableExtensions = (formatList?.filter({$0.sizeString == sender.titleOfSelectedItem}).map({$0.fileExtension.rawValue}) ?? []) + ["Auto"]
+        
+        if !availableExtensions.contains(formatsPopUpButton.titleOfSelectedItem!) {
+            print("INCOMPATIBLE: \(sender.titleOfSelectedItem) - \(resolutionPopUpButton.titleOfSelectedItem)")
+        }
     }
     
     func setURLState(_ state: URLState) {
@@ -46,11 +68,12 @@ class FormatControlsVC: NSViewController {
     }
     
     func display(formats: [MediaFormat]) {
-        formatList = formats.map({$0.fileExtension.rawValue})
+        formatList = formats
+        extensionList = formats.map({$0.fileExtension.rawValue})
         resolutionList = formats.sorted(by: {$0.size?.height ?? 0 < $1.size?.height ?? 0}).filter({!$0.audioOnly}).map({$0.sizeString ?? ""})
         
         formatsPopUpButton.removeAllItems()
-        formatsPopUpButton.addItems(withTitles: ["Auto"] + (formatList ?? []))
+        formatsPopUpButton.addItems(withTitles: ["Auto"] + (extensionList ?? []))
         resolutionPopUpButton.removeAllItems()
         resolutionPopUpButton.addItems(withTitles: ["Auto"] + (resolutionList ?? []))
     }
